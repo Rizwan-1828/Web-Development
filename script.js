2dc0a7097cf87f7ba1bf4701919197979c826e8a
Rizwan <srizwan9423@gmail.com>
2023-09-22 23:06:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const accountId = 138
+let accountEmail = "138gmail.com"
+var accountPassword = "43263"
+accountCity = "Mumbai"
+
+// accountId = 145
+accountEmail = "sriz@.com"
+accountPassword = "12343"
+accountCity = "Chennai"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
